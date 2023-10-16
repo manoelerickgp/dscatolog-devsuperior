7e2acd90ca833ff862cdb97b9c3729a1ed0d9243
manoelerickgp <manoel.computacao@gmail.com>
2023-10-15 23:45:27 -0300

    Adding product service

---
+++ backend/src/main/java/com/project/dscatolog/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.project.dscatolog.services;
+
+import com.project.dscatolog.entities.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<Product> findAllProducts() {
+        return null;
+    }
+
+    public Product findById(Long id) {
+        return null;
+    }
+
+    public Product save(Product product) {
+        return null;
+    }
+
+    public void delete(Long id) {
+    }
+
+    public Product update(Long id, Product product){
+        return null;
+    }
+}
